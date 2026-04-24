d1ea2fba9477e9b8e463d968b8e65415e2c16e3d
Arshia Baferani <272900.usn.no>
2026-04-25 00:39:19 +0200

    adda på stopper om det kommer noe feil og tester

--- backend/ais_fetcher.py
+++ backend/ais_fetcher.py
@@ -10,7 +10,7 @@ def formater_tidspunkt(tidspunkt: datetime) -> str:
     return tidspunkt.strftime("%Y%m%d%H%M")
 
 
-def hent_raa_ais_data(
+def hent_rå_ais_data(
     bbox: str,
     start: datetime,
     slutt: datetime,
@@ -26,3 +26,41 @@ def hent_raa_ais_data(
         "end": formater_tidspunkt(slutt),
         "minSpeed": min_fart,
     }
+
+
+
+
+
+  # Sender selve forespørselen til API-et
+    svar = requests.post(
+        url,
+        json=foresporsel_data,
+        timeout=innstillinger.foresporsel_timeout_sekunder,
+    )
+
+    # Hvis svar = feil, stopper programmet her
+    svar.raise_for_status()
+
+   
+    return svar.json()
+
+
+if __name__ == "__main__":
+   #test
+    slutt = datetime.now(timezone.utc)
+    start = slutt - timedelta(hours=1)
+
+    # test rundt Oslofjorden
+    test_bbox = "10.50,59.70,10.90,60.00"
+
+    data = hent_rå_ais_data(
+        bbox=test_bbox,
+        start=start,
+        slutt=slutt,
+    )
+
+
+    print("Svar fra AIS-API:")
+    print("success:", data.get("success"))
+    print("melding:", data.get("msg"))
+    print("antall punkter:", len(data.get("data", [])))
